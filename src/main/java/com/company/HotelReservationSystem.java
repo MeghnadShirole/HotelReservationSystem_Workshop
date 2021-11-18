dd06ecdf93a181eec68340d5b154be8bdf926f48
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-18 08:31:23 +0530

    Added:Welcome Message for Hotel Reservation System

---
+++ src/main/java/com/company/HotelReservationSystem.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class HotelReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Hotel Reservation System");
+    }
+}
